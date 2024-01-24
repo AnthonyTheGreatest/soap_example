b3d1f3addb17d3c8cddfe68e7a608077ee2a3c98
Miklós Milosevits <milosevitsmilos@gmail.com>
2024-01-24 08:29:56 +0100

    make progress

---
+++ dataAlapTablak.js
@@ -0,0 +1,94 @@
+// TODO: Finish data obj.
+
+export const data = {
+    ATCKONYV: {
+        SOAPAction: 'TABATC',
+        xmlData: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:pup="http://xmlns.oracle.com/orawsv/PUPHAX/PUPHAXWS">
+                    <soapenv:Header/>
+                    <soapenv:Body>
+                    <pup:COBJALAP-TABATCInput>
+                        <pup:SXFILTER-VARCHAR2-IN>
+                            <![CDATA[
+                                <alapfilter>
+                                    <ATC>%</ATC>
+                                </alapfilter>
+                            ]]>
+                        </pup:SXFILTER-VARCHAR2-IN>
+                    </pup:COBJALAP-TABATCInput>
+                    </soapenv:Body>
+                </soapenv:Envelope>`,
+        columns: ['ATC', 'HATOANYAG', 'MEGNEV']
+    },
+    ISOKONYV: {
+        SOAPAction: 'TABISO',
+        xmlData: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:pup="http://xmlns.oracle.com/orawsv/PUPHAX/PUPHAXWS">
+                    <soapenv:Header/>
+                    <soapenv:Body>
+                    <pup:COBJALAP-TABISOInput>
+                        <pup:SXFILTER-VARCHAR2-IN>
+                            <![CDATA[
+                                <alapfilter>
+                                    <ISO>%</ISO>
+                                </alapfilter>
+                            ]]>
+                        </pup:SXFILTER-VARCHAR2-IN>
+                    </pup:COBJALAP-TABISOInput>
+                    </soapenv:Body>
+                </soapenv:Envelope>`,
+        columns: ['ISO', 'MEGNEVEZES'] // 3. 'oszlop' üres
+    },
+    BNOKODOK: {
+        SOAPAction: 'TABBNO',
+        xmlData: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:pup="http://xmlns.oracle.com/orawsv/PUPHAX/PUPHAXWS">
+                    <soapenv:Header/>
+                    <soapenv:Body>
+                    <pup:COBJALAP-TABBNOInput>
+                        <pup:SXFILTER-VARCHAR2-IN>
+                            <![CDATA[
+                                <alapfilter>
+                                    <BNO>%</BNO>
+                                </alapfilter>
+                            ]]>
+                        </pup:SXFILTER-VARCHAR2-IN>
+                    </pup:COBJALAP-TABBNOInput>
+                    </soapenv:Body>
+                </soapenv:Envelope>`,
+        columns: ['ID', 'KOD', 'LEIRAS']
+    },
+    BRAND: {
+        SOAPAction: 'TABBRAND',
+        xmlData: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:pup="http://xmlns.oracle.com/orawsv/PUPHAX/PUPHAXWS">
+                    <soapenv:Header/>
+                    <soapenv:Body>
+                    <pup:COBJALAP-TABBRANDInput>
+                        <pup:SXFILTER-VARCHAR2-IN>
+                            <![CDATA[
+                                <alapfilter>
+                                    <BRAND>%</BRAND>
+                                </alapfilter>
+                            ]]>
+                        </pup:SXFILTER-VARCHAR2-IN>
+                    </pup:COBJALAP-TABBRANDInput>
+                    </soapenv:Body>
+                </soapenv:Envelope>`,
+        columns: ['ID', 'NEV'] // 3. 'oszlop' üres
+    },
+    CEGEK: {
+        SOAPAction: 'TABCEGEK',
+        xmlData: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:pup="http://xmlns.oracle.com/orawsv/PUPHAX/PUPHAXWS">
+                    <soapenv:Header/>
+                    <soapenv:Body>
+                    <pup:COBJALAP-TABCEGEKInput>
+                        <pup:SXFILTER-VARCHAR2-IN>
+                            <![CDATA[
+                                <alapfilter>
+                                    <CEGNEV>%</CEGNEV>
+                                </alapfilter>
+                            ]]>
+                        </pup:SXFILTER-VARCHAR2-IN>
+                    </pup:COBJALAP-TABCEGEKInput>
+                    </soapenv:Body>
+                </soapenv:Envelope>`,
+        columns: ['ID', 'NEV', 'ERV_KEZD_PLUS_ERV_VEGE'] // 3.'oszlop': CEGEK.ERV_KEZD és CEGEK.ERV_VEGE oszlopokból képezett karakterlánc, amely a következő alakú: ’2009.03.21-2014.12.31’
+    }
+};
---
+++ makeRequestAlapTablak.js
@@ -0,0 +1,42 @@
+// •	TabATC ATC tábla lekérdezése,
+// •	TabISO ISO tábla lekérdezése,
+// •	TabBNO  BNO tábla lekérdezése,
+// •	TabBrand BRAND tábla lekérdezése,
+// •	TabCegek CEGEK tábla lekérdezése,
+// •	TabKiintOr KIINTOR tábla lekérdezése,
+// •	TabOSzakKep SZAKVKODOK tábla lekérdezése,
+// •	TabOrvKep ORVOSOK tábla lekérdezése.
+
+import DigestClient from 'digest-fetch';
+import dotenv from 'dotenv';
+dotenv.config();
+import {data} from './dataAlapTablak.js';
+
+const client = new DigestClient(
+  process.env.SOAP_USERNAME,
+  process.env.SOAP_PASSWORD
+);
+
+const url = 'https://puphax.neak.gov.hu/PUPHAXWS';
+
+export const makeRequest = async ({SOAPAction, xmlData}) => {
+  try {
+    const response = await client.fetch(url, {
+      method: 'POST',
+      body: xmlData,
+      headers: {
+        'Accept-Encoding': 'gzip,deflate',
+        'Content-Type': 'text/xml;charset=UTF-8',
+        SOAPAction: SOAPAction,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.text();
+  } catch (error) {
+    console.log('Error making the request:', error);
+  }
+};
+
+console.log(await makeRequest(data.ATCKONYV));
