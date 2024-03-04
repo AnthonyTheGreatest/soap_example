d1736ca1d9107572f5b7cee04d241dd686b19390
Miklós Milosevits <milosevitsmilos@gmail.com>
2024-03-04 03:04:22 +0100

    10000 termek IDs processed.

--- makeRequest.js
+++ makeRequest.js
@@ -12,25 +12,35 @@ const client = new DigestClient(
 const url = 'https://puphax.neak.gov.hu/PUPHAXWS';
 
 export const makeRequest = async ({ SOAPAction, xmlData }) => {
-  try {
-    const response = await client.fetch(url, {
-      method: 'POST',
-      body: xmlData,
-      headers: {
-        'Accept-Encoding': 'gzip,deflate',
-        'Content-Type': 'text/xml;charset=UTF-8',
-        SOAPAction: SOAPAction,
-      },
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+  const maxRetries = 5;
+  const retryInterval = 5000;
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    try {
+      const response = await client.fetch(url, {
+        method: 'POST',
+        body: xmlData,
+        headers: {
+          'Accept-Encoding': 'gzip,deflate',
+          'Content-Type': 'text/xml;charset=UTF-8',
+          SOAPAction: SOAPAction,
+        },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const buffer = await response.arrayBuffer();
+      const decoder = new TextDecoder('iso-8859-2');
+      const decoded = decoder.decode(buffer);
+      return decoded;
+      // return response.text();
+    } catch (error) {
+      console.log(`Attempt ${attempt} failed. Error: ${error.message}`);
+      if (attempt < maxRetries) {
+        console.log(`Retrying in ${retryInterval / 1000} seconds...`);
+        await new Promise(resolve => setTimeout(resolve, retryInterval));
+      } else {
+        console.log('Maximum retries reached. Error making the request:', error);
+      }
     }
-    const buffer = await response.arrayBuffer();
-    const decoder = new TextDecoder('iso-8859-2');
-    const decoded = decoder.decode(buffer);
-    return decoded;
-    // return response.text();
-  } catch (error) {
-    console.log('Error making the request:', error);
   }
 };
